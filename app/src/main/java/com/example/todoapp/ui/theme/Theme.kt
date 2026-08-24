36d31a63fcd9f4229fee2f9e7938c4dcd2f20ba0
Eetu <99178022+E3TU@users.noreply.github.com>
2026-08-25 00:33:40 +0300

    Disabled material you dynamic color

--- app/src/main/java/com/example/todoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/Theme.kt
@@ -91,14 +91,14 @@ private val lightScheme = lightColorScheme(
 fun TodoAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+//    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> darkScheme
         else -> lightScheme

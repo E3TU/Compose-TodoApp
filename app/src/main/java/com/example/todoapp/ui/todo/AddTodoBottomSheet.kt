64a0996d4464c20a642facea51486a75800dd3e5
Eetu <99178022+E3TU@users.noreply.github.com>
2026-09-14 00:12:42 +0300

    Bottomsheet behavior and height updated

--- app/src/main/java/com/example/todoapp/ui/todo/AddTodoBottomSheet.kt
+++ app/src/main/java/com/example/todoapp/ui/todo/AddTodoBottomSheet.kt
@@ -3,13 +3,22 @@ package com.example.todoapp.ui.todo
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imeNestedScroll
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetDefaults
@@ -21,6 +30,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,13 +38,22 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun AddTodoBottomSheet(
     onDismiss: () -> Unit
 ) {
+    val titleState = rememberTextFieldState()
+    val descriptionState = rememberTextFieldState()
+
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+
     ModalBottomSheet(
         onDismissRequest = onDismiss,
+        sheetState = sheetState,
+        contentWindowInsets = { WindowInsets(0) },
         containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
         dragHandle = {
             BottomSheetDefaults.DragHandle(
@@ -43,24 +62,28 @@ fun AddTodoBottomSheet(
         }
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .statusBarsPadding()
+                .imePadding()
+                .navigationBarsPadding()
+                .imeNestedScroll()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
+            Box(modifier = Modifier.fillMaxWidth()) {
                 Text(
                     text = "Add Todo",
                     fontWeight = FontWeight.Bold,
                     fontSize = 20.sp,
                     modifier = Modifier
-                        .align(Alignment.TopCenter)
-                        .padding(top = 12.dp)
+                        .align(Alignment.Center)
+                        .padding(vertical = 12.dp)
                 )
-
                 IconButton(
                     onClick = onDismiss,
-                    modifier = Modifier.align(Alignment.TopEnd)
+                    modifier = Modifier.align(Alignment.CenterEnd)
                 ) {
                     Icon(
                         imageVector = Icons.Default.Close,
@@ -69,34 +92,26 @@ fun AddTodoBottomSheet(
                     )
                 }
             }
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier
-                        .height(200.dp)
-                        .width(280.dp)
-                ) {
-                    OutlinedTextField(
-                        shape = RoundedCornerShape(16.dp),
-                        state = rememberTextFieldState(initialText = ""),
-                        label = { Text("Title")}
-                    )
-                    OutlinedTextField(
-                        shape = RoundedCornerShape(16.dp),
-                        state = rememberTextFieldState(initialText = ""),
-                        label = { Text("Description") }
-                    )
 
-                    Button(
-                        onClick = {},
-                        modifier = Modifier
-                            .align(Alignment.Start)
-                    ) {
-                        Text("Apply")
-                    }
+            Column(
+                modifier = Modifier.width(280.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                OutlinedTextField(
+                    shape = RoundedCornerShape(16.dp),
+                    state = titleState,
+                    label = { Text("Title") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    shape = RoundedCornerShape(16.dp),
+                    state = descriptionState,
+                    label = { Text("Description") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(onClick = {
+                }) {
+                    Text("Apply")
                 }
             }
         }

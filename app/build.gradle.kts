0c88f2d1788139dd51d28fb4c5c5cc5c0faa15fc
Eetu <99178022+E3TU@users.noreply.github.com>
2026-09-17 00:15:23 +0300

    Added ksp, room etc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
@@ -55,4 +56,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.10.0")
     implementation("androidx.compose.material:material-icons-extended")
 
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
 }
